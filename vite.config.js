220a82258b3d97d36d9f63ca4c271f010d1803ab
Aexl <axellavignepro@gmail.com>
2024-09-05 13:25:07 +0200

    fixed for old dir replacement

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,42 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
+
+let oldtargets = [
+
+  {
+    src: 'old/*', // The source directory containing your HTML files
+    dest: '.'  // The directory in the dist folder where files will be copied
+  },
+  // {
+  //   src: 'old/Skull', // The source directory containing your HTML files
+  //   dest: '.'  // The directory in the dist folder where files will be copied
+  // },
+  // {
+  //   src: 'old/Suika', // The source directory containing your HTML files
+  //   dest: '.'  // The directory in the dist folder where files will be copied
+  // },
+  // {
+  //   src: 'old/Suikasteroid', // The source directory containing your HTML files
+  //   dest: '.'  // The directory in the dist folder where files will be copied
+  // },
+  // {
+  //   src: 'old/Suicalculator', // The source directory containing your HTML files
+  //   dest: '.'  // The directory in the dist folder where files will be copied
+  // },
+]
+
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(),
+    viteStaticCopy({
+      targets: oldtargets
+    })
+
+  ],
+
 })
 
