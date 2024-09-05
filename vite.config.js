ea6d084d53de2a45d2bcc84a2a68a84f72d9a38e
Aexl <axellavignepro@gmail.com>
2024-09-05 09:29:10 +0200

    Updated build dir

--- vite.config.js
+++ vite.config.js
@@ -4,8 +4,5 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [svelte()],
-  build: {
-    outDir: 'C:/Users/Axel/Documents/Main/Web/Websites/Aexll.github.io/home', // Specify your desired output directory here
-  },
 })
 
